6eb9f7dc0e56f91f0afdf8fd90df048d946989fd
noelballon <nballon@yahoo.com>
2016-04-27 11:23:09 -0700

    added Hulk Ryu code

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         $('.ryu-still').hide();
         $('.ryu-ready').show();
     })
-    .mouseleave(function() {
+    .mouseleave(function(){
         $('.ryu-still').show();
         $('.ryu-ready').hide();
     })
@@ -18,16 +18,42 @@ $(document).ready(function(){
                 $(this).hide();
                 $(this).css('left', '650px');
             });
+       
     })
     .mouseup(function(){
         $('.ryu-throwing').hide();
         $('.ryu-ready').show();
     });
 
+    $('.hulk-ryu').mouseenter(function(){
+        $('.hulk-ryu-still').hide();
+        $('.hulk-ryu-ready').show();
+    })
+    .mouseleave(function(){
+        $('.hulk-ryu-still').show();
+        $('.hulk-ryu-ready').hide();
+    })
+    .mousedown(function(){
+        playHadouken();
+        $('.hulk-ryu-ready').hide();
+        $('.hulk-ryu-throwing').show();
+        $('.hulk-hadouken').finish().show().animate(
+            {'right': '1150px'},
+            500,
+            function(){
+                $(this).hide();
+                $(this).css('right', '650px');
+            });
+    })
+    .mouseup(function(){
+        $('.hulk-ryu-throwing').hide();
+        $('.hulk-ryu-ready').show();
+    });
+
 });
 
 function playHadouken() {
-    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].volume = 0.1;
     $('#hadouken-sound')[0].load();
     $('#hadouken-sound')[0].play();
 }
